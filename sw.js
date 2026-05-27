203a093c363554a36f37d96aac92a20993fe1210
Scott <scott.roberts.crna@gmail.com>
2026-05-27 09:36:50 -0500

    Add Break/Lunch header pills in snapshot LSC Doc row, change lunch emoji from bento box to sandwich

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v23';
+const CACHE_NAME = 'lawrence-anaesthesia-v24';
 
 const APP_SHELL = [
   './portal.html',
