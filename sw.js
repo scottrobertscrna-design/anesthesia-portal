d231f62eb1019191c071f1e3f06de5cce5fc1ba1
Scott <scott.roberts.crna@gmail.com>
2026-05-28 10:51:33 -0500

    Optimize date calculations and bump sw to v45

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v44';
+const CACHE_NAME = 'lawrence-anaesthesia-v45';
 
 const APP_SHELL = [
   './portal.html',
