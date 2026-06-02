3e19411b0416e93906fdaafd50a976d53c3662bd
Scott <scott.roberts.crna@gmail.com>
2026-06-02 09:22:47 -0500

    Hide switches when times are set, add click-to-clear time prompt, cache bump v92

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v91';
+const CACHE_NAME = 'lawrence-anaesthesia-v92';
 
 const APP_SHELL = [
   './portal.html',
