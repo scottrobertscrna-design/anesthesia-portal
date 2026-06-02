d532dad656e7d3884153297ce3061b8ee6a5ec66
Scott <scott.roberts.crna@gmail.com>
2026-06-02 11:47:02 -0500

    Bump service worker cache to v95 to trigger client reloading of layout fixes

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v94';
+const CACHE_NAME = 'lawrence-anaesthesia-v95';
 
 const APP_SHELL = [
   './portal.html',
