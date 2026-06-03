3797e4543d76b26d4bba6989d3ed2e7999612258
Scott <scott.roberts.crna@gmail.com>
2026-06-02 21:18:20 -0500

    chore: bump service worker cache version to v104 and registration to v96

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v103';
+const CACHE_NAME = 'lawrence-anaesthesia-v104';
 
 const APP_SHELL = [
   './portal.html',
