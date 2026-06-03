4d480630b24522a6ae0a73e981899938ae95310a
Scott <scott.roberts.crna@gmail.com>
2026-06-02 21:37:37 -0500

    Bump service worker cache and visual version to v70

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v106';
+const CACHE_NAME = 'lawrence-anaesthesia-v107';
 
 const APP_SHELL = [
   './portal.html',
