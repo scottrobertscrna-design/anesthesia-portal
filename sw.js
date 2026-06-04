1eb4cef7dfca077cad30d24ca0911e5a2cf7fecb
Scott <scott.roberts.crna@gmail.com>
2026-06-04 09:05:51 -0500

    Implement transparent time input overlay on top of pills for true 1-click native time selection

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v114';
+const CACHE_NAME = 'lawrence-anaesthesia-v115';
 
 const APP_SHELL = [
   './portal.html',
