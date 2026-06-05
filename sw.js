baabba4f2959214197a99f480ba3679fe29327ca
Scott <scott.roberts.crna@gmail.com>
2026-06-04 22:24:38 -0500

    Timecard speed improvements frontend

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v123';
+const CACHE_NAME = 'lawrence-anaesthesia-v124';
 
 const APP_SHELL = [
   './portal.html',
