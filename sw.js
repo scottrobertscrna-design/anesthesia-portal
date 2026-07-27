962749ae742baf6d7b5dfe7e8e576163ead69533
Scott <scott.roberts.crna@gmail.com>
2026-07-27 10:40:43 -0500

    Bump SW cache to v176 and fix parseFlexibleDate_ 12:00:00 Central Time parsing

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v175';
+const CACHE_NAME = 'lawrence-anaesthesia-v176';
 
 const APP_SHELL = [
   './portal.html',
