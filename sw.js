bbb64c53610e029918df68a706b844b5f85b8757
Scott <scott.roberts.crna@gmail.com>
2026-09-21 20:00:26 -0500

    Reorder preferences tabs and implement 0ms optimistic caching

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  *  - Offline fallback                 → Show offline.html if network & cache both miss
  */
 
-const CACHE_NAME = 'lawrence-anaesthesia-v253';
+const CACHE_NAME = 'lawrence-anaesthesia-v254';
 
 const APP_SHELL = [
   './portal.html',
